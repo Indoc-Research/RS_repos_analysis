d463f80fd1a654539a13ab9b3fb00d09dc8c8526
ELGarulli <85746126+ELGarulli@users.noreply.github.com>
2025-01-19 13:30:42 +0100

    added additional piecharts for readme and fair principle

--- repo_metrics/get_pip_repo_combo.py
+++ repo_metrics/get_pip_repo_combo.py
@@ -5,11 +5,13 @@ import numpy as np
 if __name__ == "__main__":
 
     dataset = pd.read_csv("../../rs_usage/data/metadata.tsv", sep="\t", low_memory=False)
-    df_github = dataset[dataset["source"] == "Github API"]
+    metadata = pd.read_csv("../../rs_usage/metadata/metadata.csv", low_memory=False)
+    df_github = metadata.copy()
     df_pypi = dataset[dataset["source"] == "Pypi Index"]
     df_pypi["github_repo"] = df_pypi["github_repo"].apply(ast.literal_eval)
     df_pypi["github_url_str"] = df_pypi["github_repo"].apply(lambda lst: lst[0] if lst else None )
     pypi_repo_urls = set(df_pypi["github_url_str"].dropna())
-    df_github["exists_in_pypi"] = df_github["github_repo"].isin(pypi_repo_urls)
-    minimal_info = df_github[["software_mention", "github_repo", "exists_in_pypi"]]
+    df_github["exists_in_pypi"] = df_github["html_url"].isin(pypi_repo_urls)
+    df_github["language"] = metadata["language"]
+    minimal_info = df_github[["name", "html_url", "exists_in_pypi", "language"]]
     minimal_info.to_csv("../data/pip_repo_combo.csv")
